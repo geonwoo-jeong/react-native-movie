b46e35179d99dc92faa43b5605e8afaff25935ca
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2021-11-20 21:44:00 +0900

    AppLoading 1

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import AppLoading from 'expo-app-loading';
+import { Text }from 'react-native'
+
+export default function App() {
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    await new Promise((resolve => setTimeout(resolve, 10000)))
+  }
+  if (!ready) {
+    return <AppLoading startAsync={startLoading} onFinish={onFinish} onError={console.log}/>
+  }
+  return <Text>AppLoading</Text>;
+}
+
